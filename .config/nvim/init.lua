12c9ce7ff10d4063b9640b69cad69ad9189f220f
Sage Baram <sagebaram.career@gamil.com>
2023-02-13 00:59:19 +0200

    (ref) move impatient to core init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,5 @@
 local pass, err = pcall(require, "core")
 
-require("impatient")
 if not pass then
-    error("Error loading core configuration" .. "\n\n" .. err)
+	error("Error loading core configuration" .. "\n\n" .. err)
 end
