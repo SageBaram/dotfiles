a715f2c31744583ab811293abff960cd5a2758bf
Sage Baram <sagebaram.career@gamil.com>
2023-02-13 00:58:17 +0200

    (ref) move impatient to core init

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -29,6 +29,7 @@ return require("packer").startup(function(use)
 
 	use({
 		"kyazdani42/nvim-web-devicons",
+		event = "VimEnter",
 		config = function()
 			require("plugins.configs.icons")
 		end,
