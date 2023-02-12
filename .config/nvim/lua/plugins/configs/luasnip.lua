5b5b8798393a622d511a86cb50a8985c4d82468c
Sage Baram <sagebaram.career@gamil.com>
2023-02-13 01:24:12 +0200

    (fix) path to loaders fixed

--- .config/nvim/lua/plugins/configs/luasnip.lua
+++ .config/nvim/lua/plugins/configs/luasnip.lua
@@ -1,11 +1,11 @@
 local present, luasnip = pcall(require, "luasnip")
 if not present then
-    return
+	return
 end
 
 luasnip.config.set_config({
-    history = true,
-    updateevents = "TextChanged,TextChangedI",
+	history = true,
+	updateevents = "TextChanged,TextChangedI",
 })
 
-require("luasnip/loaders/from_vscode").load()
+require("luasnip.loaders.from_vscode").lazy_load()
