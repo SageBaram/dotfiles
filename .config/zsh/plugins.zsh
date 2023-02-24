83def06c7777c66f5406fc1847a6fdc89eac6d9d
Sage Baram <sagebaram.career@gamil.com>
2023-02-24 15:53:15 +0200

    (feat) init zoxide & zsh-abbreviations

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,12 +1,10 @@
 # syntax highlighting and autosuggestions
 source /usr/local/opt/zsh-syntax-highlighting/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /usr/local/opt/zsh-autosuggestions/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/local/opt/zsh-abbr/share/zsh-abbr/zsh-abbr.zsh
 
 # vi mode
 source $ZDOTDIR/vi-mode.zsh
-
-# activating z
-. /usr/local/etc/profile.d/z.sh
 
 # fzf loader
 [ -f $XDG_CONFIG_HOME/zsh/.fzf.zsh ] && source $XDG_CONFIG_HOME/zsh/.fzf.zsh 
@@ -17,4 +15,5 @@ eval "$(direnv hook zsh)"
 # prompt
 eval "$(starship init zsh)"
 
-
+# zoxide
+eval "$(zoxide init zsh)"
