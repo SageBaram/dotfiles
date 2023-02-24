2549bb1dda4bb44a6f2a1c32cffbee602ed353d7
Sage Baram <sagebaram.career@gamil.com>
2023-02-24 16:00:19 +0200

    (feat) add abbreviations, rem aliases

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -9,11 +9,8 @@ alias la='exa -laF --no-time --no-user --no-filesize --icons --git'
 alias lm='exa -lhamF --time-style=long-iso --no-user --no-filesize --icons --git'
 alias lt='exa --tree --level=2 --icons'                                         # tree view
 
-alias ide="$HOME/dev/scripts/ide.sh"
 alias src="source $ZDOTDIR/.zshrc"
 alias erc="vim $ZDOTDIR/.zshrc"
-alias mux="tmuxinator"
 alias fig='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias vimf='GIT_DIR=$HOME/.cfg GIT_WORK_TREE=$HOME nvim'
 alias yarn='yarn --use-yarnrc "$XDG_CONFIG_HOME/yarn/config"'
-alias createnv='python3 -m venv'
