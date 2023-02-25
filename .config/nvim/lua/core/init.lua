e8008f9dfd3759b068bd6db5eb72c8d60d9c24b4
Sage Baram <sagebaram.career@gamil.com>
2023-02-25 14:50:24 +0200

    (ref) remove impatient calls

--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -5,12 +5,6 @@ local modules = {
 	"core.commands",
 }
 
-local _impatient, impatient = pcall(require, "impatient")
-if not _impatient then
-	return false
-end
-impatient.enable_profile()
-
 for _, module in ipairs(modules) do
 	local pass, err = pcall(require, module)
 	if not pass then
