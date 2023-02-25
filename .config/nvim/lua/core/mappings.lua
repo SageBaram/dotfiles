0ff17d678ec4a2e8a42b4df0a6b175eebbc99738
Sage Baram <sagebaram.career@gamil.com>
2023-02-25 14:48:05 +0200

    (ref) add mappings, fix gitsigns

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -5,7 +5,7 @@ local function map(mode, lhs, rhs, opts) -- keybinding convention
 	if opts then
 		options = vim.tbl_extend("force", options, opts)
 	end
-	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	vim.keymap.set(mode, lhs, rhs, options)
 end
 
 -- Disable command history list on q:
@@ -24,8 +24,8 @@ map("n", "<leader>d", "\"_d")
 map("n", "<C-q>", ":lua require'core.functions'.toggle_quickfix()<CR>")
 map("n", "<C-n>", ":cnext<CR>zz")
 map("n", "<C-p>", ":cprev<CR>zz")
-map("n", "<C-f>", ":lnext<CR>zz")
-map("n", "<C-b>", ":lprev<CR>zz")
+map("n", "<C-n>", ":lnext<CR>zz")
+map("n", "<C-p>", ":lprev<CR>zz")
 
 -- Cursor standstill
 map("n", "n", "nzzzv")
@@ -40,8 +40,8 @@ map("v", "K", ":m '<-2<CR>gv=gv")
 map("n", "<leader>j", ":m .+1<CR>==")
 map("n", "<leader>k", ":m .-2<CR>==")
 
-vim.keymap.set("n", "<leader>/", ":%s/<C-r><C-w>//gI<Left><Left><Left>")
-vim.keymap.set("n", "<leader>?", ":%s/\\<<C-r><C-w>\\>//gI<Left><Left><Left>")
+map("n", "<leader>/", ":%s/<C-r><C-w>//gI<Left><Left><Left>", { silent = false })
+map("n", "<leader>?", ":%s/\\<<C-r><C-w>\\>//gI<Left><Left><Left>", { silent = false })
 map("n", "<leader>ch", ":!chmod +x %<CR>")
 
 -- Better undo - undos to punctuation
@@ -54,55 +54,61 @@ map("i", "?", "?<c-g>u")
 map("n", "<Esc>", ":noh<CR>")
 
 -- Harpoon
+map("n", "<leader>m", function() require("harpoon.mark").add_file() end)
+map("n", "<C-m>,", function() require("harpoon.ui").toggle_quick_menu() end)
+map("n", "<C-m>", ":lua require'core.functions'.nav_to_specific()<CR>")
 
 -- Fugitive
 map("n", "<leader>gs", ":Git<CR>")
-map("n", "gdf", ":diffget //2<CR>")
-map("n", "gdj", ":diffget //3<CR>")
 map("n", "<leader>gb", ":GBrowse<CR>")
 map("n", "<leader>gv", ":Gdiff<CR>")
+map("n", "gdf", ":diffget //2<CR>")
+map("n", "gdj", ":diffget //3<CR>")
 
-map("n", "<Leader>'", ":lua require('neogen').generate()<CR>")
+map("n", "<leader>'", ":lua require('neogen').generate()<CR>")
 
-vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>")
+map("n", "<leader>ut", "<Cmd>UndotreeToggle<CR>")
 
 -- Telescope
-vim.api.nvim_create_user_command("Dotfiles", function()
-	require("telescope.builtin").find_files({ cwd = "~/.config/", prompt_title = "< Dotfiles >", hidden = true })
-end, {})
-vim.keymap.set("n", "<leader>rc", ":Dotfiles<CR>")
 
-vim.keymap.set("n", "<leader>fs", function()
+map("n", "<leader>rc", function()
+	require("telescope.builtin").find_files({ cwd = "~/.config/", prompt_title = "< Dotfiles >", hidden = true })
+end)
+
+map("n", "<leader>fs", function()
 	require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
 end)
-vim.keymap.set("n", "<leader>gc", function()
+map("n", "<leader>fo", function()
+	require("telescope.builtin").oldfiles()
+end)
+map("n", "<leader>gc", function()
 	require("telescope.builtin").git_commits()
-end, {})
-vim.keymap.set("n", "<leader>ff", function()
+end)
+map("n", "<leader>ff", function()
 	require("telescope.builtin").find_files()
-end, {})
-vim.keymap.set("n", "<leader>fb", function()
+end)
+map("n", "<leader>fb", function()
 	require("telescope.builtin").buffers()
-end, {})
-vim.keymap.set("n", "<leader>fh", function()
+end)
+map("n", "<leader>fh", function()
 	require("telescope.builtin").help_tags()
-end, {})
-vim.keymap.set("n", "<leader>fw", function()
+end)
+map("n", "<leader>fw", function()
 	require("telescope.builtin").grep_string()
-end, {})
-vim.keymap.set("n", "<C-g><C-g>", function()
+end)
+map("n", "<C-g><C-g>", function()
 	require("telescope.builtin").git_files()
-end, {})
-vim.keymap.set("n", "<leader>fg", function()
+end)
+map("n", "<leader>fg", function()
 	require("telescope.builtin").live_grep()
-end, {})
-vim.keymap.set("n", "<leader>fG", function()
+end)
+map("n", "<leader>fG", function()
 	require("telescope").extensions.live_grep_args.live_grep_args({ prompt_title = " < Live Rip Grep >" })
-end, {})
-vim.keymap.set("n", "<leader>fr", function()
+end)
+map("n", "<leader>fr", function()
 	require("telescope").extensions.file_browser.file_browser()
-end, {})
-vim.keymap.set("n", "<leader>fp", function()
+end)
+map("n", "<leader>fp", function()
 	require("telescope").extensions.projects.projects()
 end, { silent = true })
 
@@ -118,129 +124,75 @@ map("n", "<leader>xq", "<Cmd>TroubleToggle quickfix<CR>")
 map("n", "<leader>gR", "<Cmd>TroubleToggle lsp_references<CR>")
 
 -- Lspsaga
--- Lsp finder find the symbol definition implement reference
--- if there is no implement it will hide
--- when you use action in finder like open vsplit then you can
--- use <C-t> to jump back
+map("n", "gj", "<Cmd>Lspsaga peek_definition<CR>")
+map("n", "gt", "<Cmd>Lspsaga peek_type_definition<CR>")
+map("n", "gT", "<Cmd>Lspsaga goto_type_definition<CR>")
+map("n", "gd", "<Cmd>Lspsaga goto_definition<CR>")
 map("n", "gr", "<Cmd>Lspsaga lsp_finder<CR>")
-
--- Code action
+map("n", "K", "<Cmd>Lspsaga hover_doc ++quiet<CR>")
+map("n", "rn", "<Cmd>Lspsaga rename<CR>")
+map("n", "<leader>K", "<Cmd>Lspsaga hover_doc ++keep<CR>")
+map("n", "<leader>rn", "<Cmd>Lspsaga rename ++project<CR>")
+map("n", "<leader>o", "<Cmd>Lspsaga outline<CR>")
+map("n", "<leader>sl", "<Cmd>Lspsaga show_line_diagnostics<CR>")
+map("n", "<leader>sc", "<Cmd>Lspsaga show_cursor_diagnostics<CR>")
+map("n", "<leader>sb", "<Cmd>Lspsaga show_buf_diagnostics<CR>")
 map("v", "<leader>ca", "<Cmd>Lspsaga code_action<CR>")
 map("n", "<leader>ca", "<Cmd>Lspsaga code_action<CR>")
-
-map("n", "rn", "<Cmd>Lspsaga rename<CR>")
-map("n", "<leader>rn", "<cmd>Lspsaga rename ++project<CR>")
-
--- Outline
-map("n", "<leader>o", "<Cmd>Lspsaga outline<CR>")
-
--- Hover Doc
-map("n", "K", "<Cmd>Lspsaga hover_doc ++quiet<CR>")
-map("n", "<leader>K", "<Cmd>Lspsaga hover_doc ++keep<CR>")
-
--- Call hierarchy
-map("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
-map("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
-
--- Peek Definition
--- you can edit the definition file in this flaotwindow
--- also support open/vsplit/etc operation check definition_action_keys
--- support tagstack C-t jump back
-map("n", "gj", "<Cmd>Lspsaga peek_definition<CR>")
-map("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>")
-map("n", "gT", "<cmd>Lspsaga goto_type_definition<CR>")
-map("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
-
--- Show line diagnostics
--- You can pass argument ++unfocus to
--- unfocus the show_line_diagnostics floating window
-map("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
--- Show cursor diagnostics
--- Like show_line_diagnostics, it supports passing the ++unfocus argument
-map("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
--- Show buffer diagnostics
--- Diagnostic jump can use `<c-o>` to jump back
-map("n", "<leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
-
-map("n", "[e", "<Cmd>Lspsaga diagnostic_jump_prev<CR>")
-map("n", "]e", "<Cmd>Lspsaga diagnostic_jump_next<CR>")
-
+map("n", "<Leader>ci", "<Cmd>Lspsaga incoming_calls<CR>")
+map("n", "<Leader>co", "<Cmd>Lspsaga outgoing_calls<CR>")
+map("n", "[d", "<Cmd>Lspsaga diagnostic_jump_prev<CR>")
+map("n", "]d", "<Cmd>Lspsaga diagnostic_jump_next<CR>")
 -- Only jump to error
-vim.keymap.set("n", "[d", function()
+vim.keymap.set("n", "[e", function()
 	require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
 end, { silent = true })
-vim.keymap.set("n", "]d", function()
+vim.keymap.set("n", "]e", function()
 	require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
 end, { silent = true })
 
 -- DAP
-vim.keymap.set("n", "<leader>dc", function()
+map("n", "<leader>dc", function()
 	require("dap").continue()
-end, {})
-vim.keymap.set("n", "<leader>dt", function()
+end)
+map("n", "<leader>dt", function()
 	require("dap").terminate()
-end, {})
-vim.keymap.set("n", "<leader>dn", function()
+end)
+map("n", "<leader>dn", function()
 	require("dap").step_over()
-end, {})
-vim.keymap.set("n", "<leader>di", function()
+end)
+map("n", "<leader>di", function()
 	require("dap").step_into()
-end, {})
-vim.keymap.set("n", "<leader>do", function()
+end)
+map("n", "<leader>do", function()
 	require("dap").step_out()
-end, {})
-vim.keymap.set("n", "<leader>db", function()
+end)
+map("n", "<leader>db", function()
 	require("dap").toggle_breakpoint()
-end, {})
-vim.keymap.set("n", "<leader>dB", function()
+end)
+map("n", "<leader>dB", function()
 	require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-end, {})
-vim.keymap.set("n", "<leader>lp", function()
+end)
+map("n", "<leader>lp", function()
 	require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-end, {})
-vim.keymap.set("n", "<leader>dr", function()
+end)
+map("n", "<leader>dr", function()
 	require("dap").repl.toggle()
-end, {})
+end)
 
 -- GIT SIGNS
---
-local gs = package.loaded.gitsigns
-vim.keymap.set("n", "]c", function()
-	if vim.wo.diff then
-		return "]c"
-	end
-	vim.schedule(function()
-		gs.next_hunk()
-	end)
-	return "<Ignore>"
-end, { expr = true })
-
-vim.keymap.set("n", "[c", function()
-	if vim.wo.diff then
-		return "[c"
-	end
-	vim.schedule(function()
-		gs.prev_hunk()
-	end)
-	return "<Ignore>"
-end, { expr = true })
-
--- Actions
+map('n', ']h', "&diff ? ']h' : ':Gitsigns next_hunk<CR>'", {expr=true})
+map('n', '[h', "&diff ? '[h' : ':Gitsigns prev_hunk<CR>'", {expr=true})
 map("n", "<leader>hs", ":Gitsigns stage_hunk<CR>")
 map("v", "<leader>hs", ":Gitsigns stage_hunk<CR>")
+map("n", "<leader>hS", ":Gitsigns stage_buffer<CR>")
 map("n", "<leader>hr", ":Gitsigns reset_hunk<CR>")
 map("v", "<leader>hr", ":Gitsigns reset_hunk<CR>")
-map("n", "<leader>hS", ":Gitsigns stage_buffer<CR>")
-map("n", "<leader>hu", ":Gitsigns undo_stage_hunk<CR>")
 map("n", "<leader>hR", ":Gitsigns reset_buffer<CR>")
+map("n", "<leader>hu", ":Gitsigns undo_stage_hunk<CR>")
 map("n", "<leader>hp", ":Gitsigns preview_hunk<CR>")
-map("n", "<leader>tb", ":Gitsigns toggle_current_line_blame<CR>")
 map("n", "<leader>hd", ":Gitsigns diffthis<CR>")
+map('n', '<leader>hb', ":lua require'gitsigns'.blame_line{full=true}<CR>")
+map('n', '<leader>hdd',":lua require'gitsigns'.diffthis('~')<CR>")
+map("n", "<leader>tb", ":Gitsigns toggle_current_line_blame<CR>")
 map("n", "<leader>td", ":Gitsigns toggle_deleted<CR>")
-
-vim.keymap.set("n", "<leader>hb", function()
-	gs.blame_line({ full = true })
-end)
-vim.keymap.set("n", "<leader>hB", function()
-	gs.diffthis("~")
-end)
